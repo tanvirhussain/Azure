473166e7dce12b48ba00386df4ab482b77772c03
Tanvir Hussain <tanvir.hussain@veinteractive.com>
2015-12-21 19:03:43 +0000

    Added mvc

---
+++ AzureResearchAndDevBlobStorage/AzureResearchAndDevBlobStorage.Service/AssetUploader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureResearchAndDevBlobStorage.Service
+{
+    public class AssetUploader : IAssetUploader
+    {
+        const string containerName = "VeconfigImageContainer";
+
+        public string Upload(string storageAccount, string blobName, System.IO.Stream blobToUpload)
+        {
+            var cloudBlobClient = ConnectAzure(storageAccount);
+
+            // Retrieve a reference to a container.
+            var container = cloudBlobClient.GetContainerReference(containerName);
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            var blockBlob = container.GetBlockBlobReference(blobName);
+
+            blockBlob.UploadFromStream(blobToUpload);
+
+            var accessUrl = blockBlob.Uri.ToString();
+
+            return accessUrl;
+        }
+
+        private CloudBlobClient ConnectAzure(string storageAccount) 
+        {
+            return CloudStorageAccount.Parse(storageAccount).CreateCloudBlobClient();
+        }    
+    }
+}
---
+++ AzureResearchAndDevBlobStorage/AzureResearchAndDevBlobStorage.Web/Controllers/UploadImageController.cs
@@ -0,0 +1,40 @@
+﻿using AzureResearchAndDevBlobStorage.Service;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureResearchAndDevBlobStorage.Web.Controllers
+{
+    public class UploadImageController : Controller
+    {
+        private readonly IAssetUploader _assetUploader;
+
+        public UploadImageController(IAssetUploader assetUploader) 
+        {
+            _assetUploader = assetUploader;
+        }
+
+        //
+        // GET: /UploadImage/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Index(HttpPostedFileBase file) 
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the filename
+                var fileName = Path.GetFileName(file.FileName);
+
+                var uploadedUrl = _assetUploader.Upload(ConfigurationManager.AppSettings["StorageConnectionString"], fileName, file.InputStream);
+            }       
+        }
+	}
+}
