ea8cacc8dd9ad7e73424711a46202a9332b4a60a
Tanvir Hussain <tanvir.hussain@veinteractive.com>
2015-12-22 14:16:49 +0000

    Can upload to azure blob storage

--- AzureResearchAndDevBlobStorage/AzureResearchAndDevBlobStorage.Service/AssetUploader.cs
+++ AzureResearchAndDevBlobStorage/AzureResearchAndDevBlobStorage.Service/AssetUploader.cs
@@ -13,17 +13,18 @@ namespace AzureResearchAndDevBlobStorage.Service
 {
     public class AssetUploader : IAssetUploader
     {
-        const string containerName = "VeconfigImageContainer";
+        const string containerName = "veconfigcustomerimagecontainer";
 
         public string Upload(string storageAccount, string blobName, System.IO.Stream blobToUpload)
         {
             var cloudBlobClient = ConnectAzure(storageAccount);
 
             // Retrieve a reference to a container.
-            var container = cloudBlobClient.GetContainerReference(containerName);
+            var container = cloudBlobClient.GetContainerReference(containerName);           
 
             // Create the container if it doesn't already exist.
             container.CreateIfNotExists();
+            container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Container });
 
             var blockBlob = container.GetBlockBlobReference(blobName);
 
